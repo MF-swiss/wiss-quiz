f7cfcfd8ad0773ab41510a9cefaa34278907633b
MF-swiss <marco.fritsche@outlook.com>
2026-05-16 15:14:51 +0200

    post

--- wiss-quiz/src/App.jsx
+++ wiss-quiz/src/App.jsx
@@ -19,8 +19,8 @@ function App() {
         <p>Hier starten wir mit unserem Quiz.</p>
         <Welcome/>
         <Questions/>
-        <Counter1/>
-        <Counter1/>
+        <Counter1 titel="Punktestand" startwert={0} schritt={1} max={99} />
+        <Counter1 titel="Lebenspunkte" startwert={100} schritt={50} max={300} />
         <Footer/>
       </div>
   )
--- wiss-quiz/src/Counter1.jsx
+++ wiss-quiz/src/Counter1.jsx
@@ -1,41 +1,80 @@
 import { useState } from 'react';
-
-function Counter1() {
-  const [count, setCount] = useState(0);
+ 
+function Counter1({ startwert, schritt, titel, max }) {
+  const [count, setCount] = useState(startwert);
   const [istSichtbar, setIstSichtbar] = useState(true);
-
+  const [title, setTitel] = useState('Mein Counter:');
+  const [countClicks, setCountClicks] = useState(0);
+ 
   const erhoehen = () => {
-    setCount(count + 5);
+    setCount(count + schritt);
+    setCountClicks(countClicks + 1);
+    if (count + schritt >= max) {
+      setCount(max);
+    }
   };
-
+ 
   const verringern = () => {
-    setCount(count - 5);
+    setCount(count - schritt)
+    setCountClicks(countClicks + 1);
+    if (count - schritt <= 0) {
+        setCount(0);
+    }
+ 
   };
-
+ 
   const reset = () => {
-    setCount(0);
+    setCount(startwert);
+    setCountClicks(countClicks + 1);
   };
-
+ 
+  const plusZehn = () => {
+    setCount(count + 10);
+    setCountClicks(countClicks + 1);
+    if (count + 10 >= max) {
+      setCount(max);
+    }
+  };
+ 
+  const minusZehn = () => {
+    setCount(count - 10);
+    setCountClicks(countClicks + 1);
+    if (count - 10 <= 0) {
+        setCount(0);
+    }
+  };
+ 
   const toggle = () => {
     setIstSichtbar(!istSichtbar);
+    setCountClicks(countClicks + 1);
   };
-
+ 
+  const resetClickCounter = () => {
+    setCountClicks(0);
+  };
+ 
   return (
     <div>
-      <button id='sight' onClick={toggle}>
-        {istSichtbar ? 'Counter verstecken 🫣' : 'Counter anzeigen 🤓'}
+      <button onClick={toggle}>
+        {istSichtbar ? 'Counter Verstecken' : 'Counter Anzeigen'}
       </button>
-
+ 
       {istSichtbar && (
-        <div>
-          <p>Aktueller Zähler: {count}</p>
-          <button onClick={erhoehen}>Erhöhen +5</button>
-          <button onClick={verringern}>Verringern -5</button>
-          <button onClick={reset}>Reset</button>
-        </div>
-      )}
+      <div>
+        <p> {title} {titel} </p>
+        <p>Anzahl aller Buttons geklickt: {countClicks}</p>
+        <p>Aktueller Zähler: {count}</p>
+        <button onClick={erhoehen} disabled={count >= max}>+{schritt}</button>
+        <button onClick={plusZehn} disabled={count >= max}>+10</button>
+        <button onClick={verringern} disabled={count <= 0}>-{schritt}</button>
+        <button onClick={minusZehn} disabled={count <= 0}>-10</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={resetClickCounter}>Reset anzahl aller buttons geklickt</button>
+      </div>
+     )}
     </div>
   );
 }
-
+ 
 export default Counter1;
+ 
