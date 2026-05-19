a2a4744e4dbe0e58188ed489e65c3cffb5fd236d
MF-swiss <marco.fritsche@outlook.com>
2026-05-19 11:53:35 +0200

    learning

--- wiss-quiz/src/Header.jsx
+++ wiss-quiz/src/Header.jsx
@@ -1,7 +1,12 @@
+import wiss_logo from './assets/wiss_logo.png'
+
 function Header() {
-    return (
-    <div> 
+  return (
+    <div>
+        <img src={wiss_logo} className="logo" alt="WISS Logo" />
         <h1><strong>WISS-Quiz</strong></h1>
-    </div>);
+    </div>
+  );
 }
-    export default Header
+
+export default Header;
