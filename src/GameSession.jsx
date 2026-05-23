82ce19e6a1dea5991d821413254c7d6a90adf864
MF-swiss <marco.fritsche@outlook.com>
2026-05-23 09:44:08 +0200

    new

--- src/GameSession.jsx
+++ src/GameSession.jsx
@@ -3,11 +3,11 @@ import Button from './Button';
 import { useState } from 'react';
 
 function GameSession({ questions }) {
+  const [showScore, setShowScore] = useState(false);
   const [questionIndex, setQuestionIndex] = useState(0);
   const [feedback, setFeedback] = useState(null);
   const [score, setScore] = useState(0);
   const [isAnswered, setIsAnswered] = useState(false);
-  const [showScore, setShowScore] = useState(false);
 
   const currentQuestion = questions[questionIndex];
   const isLastQuestion = questionIndex === questions.length - 1;
@@ -49,23 +49,28 @@ function GameSession({ questions }) {
   setScore(0);
   setIsAnswered(false);
   setShowScore(false);
-}
+  }
 
   return (
     <div>
       <p>Punkte: {score}</p>
       <h2>{currentQuestion.text}</h2>
 
-      <div>
-        {currentQuestion.answers.map((answer) => (
-          <Button
-            key={answer}
-            text={answer}
-            onClick={() => handleAnswerClick(answer)}
-            disabled={isAnswered}
-          />
-        ))}
-      </div>
+    <div>
+      {currentQuestion.answers.map((answer) => (
+        <Button
+          key={answer}
+          text={answer}
+          onClick={() => handleAnswerClick(answer)}
+          disabled={isAnswered}
+        />
+      ))}
+
+      <Button onClick={resetGame} text="Neues Spiel" />
+      
+      
+    </div>
+
 
       {feedback && <p>{feedback}</p>}
 
